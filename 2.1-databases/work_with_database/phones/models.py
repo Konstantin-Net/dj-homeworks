a0925293db39d67198ba1f82ed65528735f7f34c
Konstantin <krasny@mail.ru>
2022-10-19 17:46:51 +0300

    First commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True, serialize=False)
+    name = models.CharField(max_length=100, verbose_name='Модель телефона')
+    price = models.IntegerField(verbose_name='Цена')
+    image = models.ImageField(upload_to='', verbose_name='Изображение')
+    release_date = models.DateField(verbose_name='Дата появления')
+    lte_exists = models.BooleanField(verbose_name='Наличие LTE')
+    slug = models.SlugField(blank=True, unique=True, verbose_name='URL')
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,24 @@ def index(request):
 
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sor = request.GET.get("sort")
+
+    def sort_phone(parameter):
+        phones = Phone.objects.all().order_by(parameter)
+        context = {"phones": phones}
+        return render(request, 'catalog.html', context)
+    if sor == 'name':
+        return sort_phone('name')
+    elif sor == 'min_price':
+        return sort_phone('price')
+    elif sor == 'max_price':
+        return sort_phone('-price')
+    else:
+        return sort_phone('id')
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {"phone": phone}
     return render(request, template, context)
